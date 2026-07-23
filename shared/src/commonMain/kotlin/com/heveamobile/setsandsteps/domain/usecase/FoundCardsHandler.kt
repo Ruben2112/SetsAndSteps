2ee9dd32fc0d00f485f908f15beda493d0da9327
Ruben2112 <ruben@vanamelsvoort.me>
2026-07-23 13:02:31 +0200

    Added missing files

---
+++ shared/src/commonMain/kotlin/com/heveamobile/setsandsteps/domain/usecase/FoundCardsHandler.kt
@@ -0,0 +1,13 @@
+package com.heveamobile.setsandsteps.domain.usecase
+
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+
+class FoundCardsHandler {
+    private val _foundCardsEvents = MutableSharedFlow<SpendStepsResult>(extraBufferCapacity = 1)
+    val foundCardsEvents = _foundCardsEvents.asSharedFlow()
+
+    fun announceFoundCards(result: SpendStepsResult) {
+        _foundCardsEvents.tryEmit(result)
+    }
+}

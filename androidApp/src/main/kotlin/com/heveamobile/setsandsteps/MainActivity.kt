c5ccc703ab39eb8c22fb847714a3457ade7912d2
Ruben2112 <ruben@vanamelsvoort.me>
2026-07-29 13:51:23 +0200

    Removed forgotten test code

--- androidApp/src/main/kotlin/com/heveamobile/setsandsteps/MainActivity.kt
+++ androidApp/src/main/kotlin/com/heveamobile/setsandsteps/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.heveamobile.setsandsteps
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,31 +13,5 @@ class MainActivity : ComponentActivity() {
         setContent {
             App()
         }
-
-        main()
-    }
-
-
-    fun main() {
-        Log.d(
-            "HENK",
-            "Start",
-        )
-        thread {
-            blockingCode()
-        }
-        Log.d(
-            "HENK",
-            "End",
-        )
-    }
-
-    fun blockingCode() {
-        (1..5_000_000).map { it * it }
-
-        Log.d(
-            "HENK",
-            "Blocking code finished!",
-        )
     }
 }

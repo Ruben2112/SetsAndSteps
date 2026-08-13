fad8016f0e7fbd81d6b7be7ae53d3144eb5e12b1
Ruben2112 <ruben@vanamelsvoort.me>
2026-08-13 10:37:50 +0200

    Added padding below CardDetailContent to prevent close button covering details

--- core/foundcards/src/commonMain/kotlin/com/heveamobile/setsandsteps/core/foundcards/FoundCardsCardDetailContent.kt
+++ core/foundcards/src/commonMain/kotlin/com/heveamobile/setsandsteps/core/foundcards/FoundCardsCardDetailContent.kt
@@ -1,9 +1,11 @@
 package com.heveamobile.setsandsteps.core.foundcards
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -24,17 +26,15 @@ internal fun CardDetailContent(
         modifier = modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium),
     ) {
         CardDetailsCard(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = MaterialTheme.spacing.medium),
-            contentPadding = PaddingValues(
-                top = MaterialTheme.spacing.extraLarge,
-                bottom = MaterialTheme.spacing.medium,
-            ),
             cardSet = state.findFoundCard(card).cardSet,
             card = card,
         )
+        Spacer(modifier = Modifier.height(MaterialTheme.spacing.extraLarge))
     }
 }

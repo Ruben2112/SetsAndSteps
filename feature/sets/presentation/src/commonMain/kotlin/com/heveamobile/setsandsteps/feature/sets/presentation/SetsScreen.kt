fed89aa82760be515c834fb7aece94df5cacadae
Ruben2112 <ruben@vanamelsvoort.me>
2026-08-28 10:41:55 +0200

    Fixed padding for sets screen

--- feature/sets/presentation/src/commonMain/kotlin/com/heveamobile/setsandsteps/feature/sets/presentation/SetsScreen.kt
+++ feature/sets/presentation/src/commonMain/kotlin/com/heveamobile/setsandsteps/feature/sets/presentation/SetsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -207,9 +208,8 @@ private fun SetsContent(
     ) { page ->
         val tab = SetsTab.entries[page]
         LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(MaterialTheme.spacing.medium),
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(MaterialTheme.spacing.medium),
             verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium),
         ) {
             when (tab) {
